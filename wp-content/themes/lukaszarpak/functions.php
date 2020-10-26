20d24c5b8642df4c8f4b4fd32a0674f083407524
lukaszarpak <szarpak90@gmail.com>
2020-10-26 11:17:34 +0100

    version bump

--- wp-content/themes/lukaszarpak/functions.php
+++ wp-content/themes/lukaszarpak/functions.php
@@ -12,7 +12,7 @@
  */
 require_once 'autoload.php';
 
-define( 'LUKASZARPAK_VERSION', '1.0.0' );
+define( 'LUKASZARPAK_VERSION', '1.0.1' );
 define( 'LUKASZARPAK_URL', get_stylesheet_directory_uri() );
 define( 'LUKASZARPAK_PATH', get_stylesheet_directory() );
 
