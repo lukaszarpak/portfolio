3786fcf9d4dd9a1511a5885f25807d3b221b2c01
lukaszarpak <szarpak90@gmail.com>
2020-10-26 19:42:42 +0100

    add background image

--- wp-content/themes/lukaszarpak/functions.php
+++ wp-content/themes/lukaszarpak/functions.php
@@ -12,7 +12,7 @@
  */
 require_once 'autoload.php';
 
-define( 'LUKASZARPAK_VERSION', '1.0.13' );
+define( 'LUKASZARPAK_VERSION', '1.0.14' );
 define( 'LUKASZARPAK_URL', get_stylesheet_directory_uri() );
 define( 'LUKASZARPAK_PATH', get_stylesheet_directory() );
 
